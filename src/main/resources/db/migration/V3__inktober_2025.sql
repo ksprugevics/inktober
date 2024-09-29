74b1dde0826d6fe8bf52f0a5909ba0aef1b50ab4
ksprugevics <klavs.dev@gmail.com>
2024-09-29 16:11:22 +0300

    Added application.properties and themes for 2024

--- src/main/resources/db/migration/V3__inktober_2025.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO event (event_id, name, start_date, end_date) VALUES (2, "Inktober 2025", date("2025-10-01"), date("2025-10-31"));
-INSERT INTO theme (description, date_for, event_id) VALUES ("Pig", "2025-10-01", 2);
-INSERT INTO theme (description, date_for, event_id) VALUES ("Bee", "2025-10-02", 2);
-INSERT INTO theme (description, date_for, event_id) VALUES ("Wolf", "2025-10-03", 2);
